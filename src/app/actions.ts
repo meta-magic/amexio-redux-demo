f9605aadeaf0bbde9dfcffb5251ecd859c9dbacd
ketangote <ketan.gote@metamagic.in>
2019-02-12 11:46:14 +0530

    Changes: Removed toggle functionality

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,4 +1,3 @@
 export const ADD_TODO = "ADD_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 export const REMOVE_ALL_TODO = "REMOVE_ALL_TODO";
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { IToDos } from "./itodos";
-import { ADD_TODO, TOGGLE_TODO, REMOVE_ALL_TODO, REMOVE_TODO } from "./actions";
+import { ADD_TODO,  REMOVE_ALL_TODO, REMOVE_TODO } from "./actions";
 
 export interface IAppState {
     todos: IToDos[];
@@ -23,17 +23,6 @@ export function rootReducer(state: IAppState, action){
                 todos: state.todos.concat(Object.assign({},action.todo)),
                 lastUpdate: new Date()
             })
-        case TOGGLE_TODO : 
-            var todo = state.todos.find(t => t.id === action.id);
-            var index = state.todos.indexOf(todo);
-            return Object.assign({}, state,{
-                todos: [
-                    ...state.todos.slice(0, index),
-                    Object.assign({},todo, { isCompleted: !todo.isCompleted }),
-                    ...state.todos.slice(index + 1),
-                  ],
-                  lastUpdate: new Date()
-            });
         case REMOVE_ALL_TODO : 
             return Object.assign({},state, {
                 todos: [],
