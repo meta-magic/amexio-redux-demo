7e549c2b725f9f427ca53306b419a055e9beb3b4
ketangote <ketan.gote@metamagic.in>
2019-03-15 18:49:42 +0530

    Changes: Update with subscribe model

--- src/app/component/priority.dashboard.component.ts
+++ src/app/component/priority.dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { select } from '@angular-redux/store';
+import { select, NgRedux } from '@angular-redux/store';
+import { IAppState } from '../store';
 
 @Component({
     selector : 'priority-dashboard',
@@ -8,6 +9,12 @@ import { select } from '@angular-redux/store';
 export class PriorityDashboardComponent {
 
     @select('severityStatus') severityStatus;
-
     
+
+    constructor(private ngRedux : NgRedux<IAppState>){
+        
+        this.ngRedux.subscribe(()=>{
+            console.log("--IN CASE YOU WANT TO BIND DATA TO MODEL DO HEAR--"+JSON.stringify(ngRedux.getState()));
+        })
+    }
 }
